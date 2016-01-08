8f6dbe3a0073c08d95c24c90d46e22031eec7c40
George Beech <george@stackoverflow.com>
2016-01-08 11:39:01 -0500

    To Put on the Githubs

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using WSManAutomation;
+
+using static System.Console;
+
+namespace QueryWSMan
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            string user = "USERNAME", password = "PASS";
+            var schema = "http://schemas.dell.com/wbem/wscim/1/cim-schema/2/DCIM_SystemInteger?__cimnamespace=root/dcim+InstanceID=System.Embedded.1#ServerTopology.1#RackSlot";
+
+
+            var wsman = new WSMan();
+            var wsManFlags = wsman.SessionFlagCredUsernamePassword() | wsman.SessionFlagSkipCNCheck() |
+                             wsman.SessionFlagSkipCACheck() | wsman.SessionFlagUseBasic() | wsman.SessionFlagUTF8();
+            var wsManOpt = (IWSManConnectionOptions) wsman.CreateConnectionOptions();
+
+
+            var wsManEndpoint = "https://co-web03-drac/wsman";
+            wsManOpt.UserName = user;
+            wsManOpt.Password = password;
+
+            var wsManSession = (IWSManSession) wsman.CreateSession(wsManEndpoint, wsManFlags, wsManOpt);
+            wsManSession.Timeout = 60000;
+            try
+            {
+                if ((wsManSession.Identify()) == null)
+                {
+                    WriteLine("Not a WSMan Endpoint");
+                    Environment.Exit(1);
+                }
+
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                WriteLine($"[Identify] : Not authroized.");
+                //Console.WriteLine($"{ip}: {e.Message}\n{e.StackTrace}");
+                Environment.Exit(1);
+            }
+            catch (COMException e)
+            {
+                WriteLine($"[Identify] : COM Exception.");
+                //Console.WriteLine($"{ip}: {e.Message}\n{e.StackTrace}");
+                Environment.Exit(1);
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("could not connect"))
+                {
+                    WriteLine($"[Identify] : Count not connect.");
+                    Environment.Exit(1);
+                }
+                WriteLine($"[Identify] : Error ({e.GetType()}):{e.Message}\n{e.StackTrace}");
+                Environment.Exit(1);
+            }
+
+            try
+            {
+                var wsInstance = wsManSession.Get(schema);
+                WriteLine(wsInstance);
+                var s = new XmlSerializer(typeof(DCIM_SystemString));
+                DCIM_SystemString obj = (DCIM_SystemString)s.Deserialize(new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(wsInstance))));
+                WriteLine(obj.CurrentValue);
+                ReadKey();
+
+            }
+            catch (Exception e)
+            {
+                WriteLine($"[Query] : Error ({e.GetType()}:{e.Message}\n{e.StackTrace}");
+                ReadKey();
+            }
+            
+            /*
+            while (!wsInstance.AtEndOfStream())
+            {
+                var curItem = wsInstance.ReadItem();
+
+                using (var r = XmlReader.Create(new StringReader(curItem.ToString())))
+                {
+                    var xd = XDocument.Load(r);
+                    WriteLine(xd.Document);
+
+                }
+            }
+            */
+            
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ RackName.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryWSMan
+{
+
+
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://schemas.dell.com/wbem/wscim/1/cim-schema/2/DCIM_SystemString")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://schemas.dell.com/wbem/wscim/1/cim-schema/2/DCIM_SystemString", IsNullable = false)]
+    public partial class DCIM_SystemString
+    {
+
+        private string attributeDisplayNameField;
+
+        private string attributeNameField;
+
+        private string currentValueField;
+
+        private object defaultValueField;
+
+        private object dependencyField;
+
+        private ushort displayOrderField;
+
+        private string fQDDField;
+
+        private string groupDisplayNameField;
+
+        private string groupIDField;
+
+        private string instanceIDField;
+
+        private bool isReadOnlyField;
+
+        private byte maxLengthField;
+
+        private byte minLengthField;
+
+        private object pendingValueField;
+
+        private string langField;
+
+        /// <remarks/>
+        public string AttributeDisplayName
+        {
+            get
+            {
+                return this.attributeDisplayNameField;
+            }
+            set
+            {
+                this.attributeDisplayNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string AttributeName
+        {
+            get
+            {
+                return this.attributeNameField;
+            }
+            set
+            {
+                this.attributeNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CurrentValue
+        {
+            get
+            {
+                return this.currentValueField;
+            }
+            set
+            {
+                this.currentValueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(IsNullable = true)]
+        public object DefaultValue
+        {
+            get
+            {
+                return this.defaultValueField;
+            }
+            set
+            {
+                this.defaultValueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(IsNullable = true)]
+        public object Dependency
+        {
+            get
+            {
+                return this.dependencyField;
+            }
+            set
+            {
+                this.dependencyField = value;
+            }
+        }
+
+        /// <remarks/>
+        public ushort DisplayOrder
+        {
+            get
+            {
+                return this.displayOrderField;
+            }
+            set
+            {
+                this.displayOrderField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string FQDD
+        {
+            get
+            {
+                return this.fQDDField;
+            }
+            set
+            {
+                this.fQDDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string GroupDisplayName
+        {
+            get
+            {
+                return this.groupDisplayNameField;
+            }
+            set
+            {
+                this.groupDisplayNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string GroupID
+        {
+            get
+            {
+                return this.groupIDField;
+            }
+            set
+            {
+                this.groupIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string InstanceID
+        {
+            get
+            {
+                return this.instanceIDField;
+            }
+            set
+            {
+                this.instanceIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public bool IsReadOnly
+        {
+            get
+            {
+                return this.isReadOnlyField;
+            }
+            set
+            {
+                this.isReadOnlyField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte MaxLength
+        {
+            get
+            {
+                return this.maxLengthField;
+            }
+            set
+            {
+                this.maxLengthField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte MinLength
+        {
+            get
+            {
+                return this.minLengthField;
+            }
+            set
+            {
+                this.minLengthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(IsNullable = true)]
+        public object PendingValue
+        {
+            get
+            {
+                return this.pendingValueField;
+            }
+            set
+            {
+                this.pendingValueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute(Form = System.Xml.Schema.XmlSchemaForm.Qualified, Namespace = "http://www.w3.org/XML/1998/namespace")]
+        public string lang
+        {
+            get
+            {
+                return this.langField;
+            }
+            set
+            {
+                this.langField = value;
+            }
+        }
+    }
+
+
+
+
+
+
+}
